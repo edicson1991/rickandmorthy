ce7b42bca07ac7728bf2ee0d19d38209a2599900
Edicson Armando Hurtado Lozano <edhurtad@Nequi.com>
2024-09-10 13:51:44 -0500

    search commit

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ const containerCharacters = document.getElementById("seccion-card");
 const prevButton = document.getElementById("prevPage");
 const nextButton = document.getElementById("nextPage");
 const pageNumberElement = document.getElementById("pageNumber");
+const searchInput = document.getElementById("search");
 
 function fetchCharacters(url) {
     fetchData(url).then((data) => {
@@ -33,5 +34,27 @@ nextButton.addEventListener("click",  () => {
     currentPageUrl = nextPageUrl; 
 } )
 
+searchInput.addEventListener('input', (e) => {
+    const query = e.target.value.toLowerCase();
+    if (query) {
+        const searchUrl = `https://rickandmortyapi.com/api/character/?name=${query}`;
+        fetchData(searchUrl)
+            .then(data => {
+                if (data.results) {
+                    const limitedResults = data.results.slice(0, 8); // Mostrar 8 resultados como límite
+                    renderCharacters(containerCharacters, limitedResults);
+                } else {
+                    containerCharacters.innerHTML = `<p>No se encontraron personajes.</p>`;
+                }
+            })
+            .catch(error => {
+                console.error('Error en la búsqueda:', error);
+                containerCharacters.innerHTML = `<p>Error al buscar personajes.</p>`;
+            });
+    } else {
+        // Si el input está vacío, vuelve a cargar la página actual
+        fetchCharacters(currentPageUrl);
+    }
+});
 
 fetchCharacters(currentPageUrl);
